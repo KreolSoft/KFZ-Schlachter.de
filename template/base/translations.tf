294a5622a39f1ed15e5eb018a69340ce374d36d4
Nikita N <igelhaut94@gmail.com>
2018-11-10 22:04:23 +0100

    Create translations.tf

---
+++ template/base/translations.tf
@@ -0,0 +1,100 @@
+		>> de
+Ersatzteile suchen...
+		>> en
+Search for parts...
+
+
+		>> de
+Suchen
+		>> en
+Search
+
+
+		>> de
+Gast
+		>> en
+Guest
+
+
+		>> de
+Zu meinem Profil
+		>> en
+Visit my Profile
+
+
+		>> de
+neue Nachrichten
+		>> en
+new messages
+
+
+		>> de
+Einloggen
+		>> en
+Login
+
+
+		>> de
+Anmelden
+		>> en
+Sign up
+
+
+		>> de
+Artikel anbieten
+		>> en
+Sell item
+
+
+		>> de
+Kontrollzentrum
+		>> en
+Dashboard
+
+
+		>> de
+Einstellungen
+		>> en
+Settings
+
+
+		>> de
+Ausloggen
+		>> en
+Logout
+
+
+		>> de
+Hilfe
+		>> en
+Help
+
+
+		>> de
+Startseite
+		>> en
+Home
+
+
+		>> de
+Angebotsübersicht
+		>> en
+See all items
+
+
+		>> de
+Brauchen Sie Hilfe?
+		>> en
+Do you need help?
+
+
+		>> de
+Nutzungsbedingungen
+		>> en
+Terms of use
+
+
+		>> de
+Datenschutzerklärung
+		>> en
+Privacy policy
