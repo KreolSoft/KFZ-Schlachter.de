9058769d872f5afb69a92d40c3b06f031d177087
Nikita N <igelhaut94@gmail.com>
2018-11-10 12:24:33 +0100

    Create translations.tf

---
+++ page/item/translations.tf
@@ -0,0 +1,292 @@
+		>> de
+Angebot wiedereinstellen
+
+
+		>> de
+Angebot bearbeiten
+
+
+		>> de
+Aus dem Verkauf nehmen
+
+
+		>> de
+Ähnlichen Artikel verkaufen
+
+
+		>> de
+Anzeigendetails
+
+
+		>> de
+Startzeit
+
+
+		>> de
+Letzte Änderung
+
+
+		>> de
+Nicht eingestellt
+
+
+		>> de
+Verkauft
+
+
+		>> de
+Aktiv
+
+
+		>> de
+Statistik
+
+
+		>> de
+Besucher
+
+
+		>> de
+Leider schon verkauft...
+		>> en
+Sadly already sold...
+
+
+		>> de
+Zur Startseite
+		>> en
+Back Home
+
+
+		>> de
+Verkaufsstatus
+		>> en
+Status
+
+
+		>> de
+Verfügbar
+		>> en
+Available
+
+
+		>> de
+Nicht eingestellt
+
+
+		>> de
+Verkauft
+		>> en
+Sold
+
+
+		>> de
+Artikelzustand
+		>> en
+Condition
+
+
+		>> de
+Neu
+		>> en
+New
+
+
+		>> de
+Neuwertig
+		>> en
+As good as new
+
+
+		>> de
+Aufbereitet/ Überholt
+		>> en
+Refurbished/ reconditioned
+
+
+		>> de
+Gebraucht
+		>> en
+Used
+
+
+		>> de
+Beschädigt
+		>> en
+Damaged
+
+
+		>> de
+Defekt
+		>> en
+Broken
+
+
+		>> de
+Sonstiges
+		>> en
+Misc
+
+
+		>> de
+Preis
+		>> en
+Price
+
+
+		>> de
+Verkäufer kontaktieren
+		>> en
+Contact seller
+
+
+		>> de
+Versand
+		>> en
+Shipping
+
+
+		>> de
+kostenlos!
+		>> en
+free!
+
+
+		>> de
+Zahlungsweise
+		>> en
+Payment methods
+
+
+		>> de
+Überweisung
+		>> en
+Bank transfer
+
+
+		>> de
+Barzahlung
+		>> en
+Cash payment
+
+
+		>> de
+Scheck
+		>> en
+Cheque
+
+
+		>> de
+Nachnahme
+		>> en
+Payment on delivery
+
+
+		>> de
+Sonstige
+		>> en
+Misc
+
+
+		>> de
+Rückgabe und Garantie
+
+
+		>> de
+Teilen
+		>> en
+Share
+
+
+		>> de
+Händler
+		>> en
+Retailer
+
+
+		>> de
+Privatverkäufer
+		>> en
+Private seller
+
+
+		>> de
+Andere Angebote des Verkäufers
+		>> en
+Sellers other items
+
+
+		>> de
+Besuche
+		>> en
+Visits
+
+
+		>> de
+Kontakt mit Verkäufer aufnehmen
+		>> en
+Contact the seller
+
+
+		>> de
+oder
+		>> en
+or
+
+
+		>> de
+Ihr Name
+		>> en
+Your name
+
+
+		>> de
+Nachricht an den Verkäufer
+		>> en
+Message to seller
+
+
+		>> de
+Wie kann Sie der Verkäufer zurück kontaktieren?
+		>> en
+How can the seller respond to you?
+
+
+		>> de
+Ihre E-Mail Adresse
+		>> en
+Your e-mail address
+
+
+		>> de
+Ihre Telefonnummer
+		>> en
+Your phone number
+
+
+		>> de
+Eine Kopie der Nachricht wird zur Bestätigung an die von Ihnen angegebene E-Mail Adresse gesendet.
+
+
+		>> de
+Herzlichen Glückwunsch!
+
+
+		>> de
+Rückgabe und Garantie
+
+
+		>> de
+Der Absendername ist obligatorisch.\nBitte geben Sie Ihren Namen ein, damit der Verkäufer weiß, wie er Sie ansprechen soll.
+
+
+		>> de
+Bitte geben eine Nachricht an den Verkäufer ein.
+
+
+		>> de
+Bitte geben Sie Ihre E-Mail Adresse ein, damit Sie der Verkäufer zurück kontaktieren kann.
+
+
+		>> de
+Es gab einen Fehler beim Versenden Ihrer Nachricht, bitte versuchen Sie es erneut oder kontaktieren Sie den Verkäufer anderweitig.
